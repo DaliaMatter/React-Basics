84edea44f8d5f65df53be015cd97ec09c465f1c7
DaliaMatter <daliaamohamed243@gmail.com>
2021-03-14 19:09:53 +0200

    remember states and update it with events with many way.

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 
 class Hello extends Component {
-    state = { 
+    constructor(props) {
+        super(props);
+        this.increment = this.increment.bind(this);
+    }
+
+    state = {
         user: {
             first_name: "Doda",
             last_name: "Dody",
             age: 24,
-        }
+        },
+    };
+    decrement = () => {
+        this.setState({ age: this.state.user.age-- });
+    };
+
+    increment1() {
+        this.setState({ age: this.state.user.age++ });
     }
-    render (props) {
+    increment() {
+        this.setState({ age: this.state.user.age++ });
+    }
+    render(props) {
         return (
             <div>
                 hello from class Component{" "}
@@ -16,11 +31,24 @@ class Hello extends Component {
                 <h1>
                     {this.state.user.first_name +
                         " " +
-                        this.state.user.last_name
-                    }
+                        this.state.user.last_name}
                     <br></br>
                     {this.state.user.age}
                 </h1>
+                <button
+                    onClick={() => {
+                        this.setState({ age: this.state.user.age++ });
+                    }}
+                >
+                    Increment
+                </button>
+                <button onClick={this.decrement}>Decrement</button> 
+                <button onClick={this.increment1.bind(this)}>
+                    Increment with fun,bind
+                </button>
+                <button onClick={this.increment}>
+                    Increment with Fun,const
+                </button>
             </div>
         );
     }
